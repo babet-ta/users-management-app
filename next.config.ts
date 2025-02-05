57df796a3e6a85753867370f7b87220564b88973
babet_ta <yourworthlesslife@icloud.com>
2025-02-05 12:44:37 +0300

    chore: update dependencies and next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig: import('next').NextConfig = {
+  sassOptions: {
+    includePaths: ['./styles'],
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
